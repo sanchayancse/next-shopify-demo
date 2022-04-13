3a3388a65d2e87c8868888ca628559bd7a4e3133
sanchayancse <sanchayandas1996@gmail.com>
2022-04-13 13:58:41 +0530

    code update

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
-
-
 module.exports = {
+  generateBuildId: () => 'build-id',
+
   env: {
-    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
-    SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN
+    STORE_FRONT_DOMAIN: process.env.STORE_FRONT_DOMAIN,
+    STORE_FRONT_ACCESS_TOKEN: process.env.STORE_FRONT_ACCESS_TOKEN,
+    NETLIFY_URL: process.env.NETLIFY_URL,
+    AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
+    AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID,
+    AIRTABLE_TABLE_NAME: process.env.AIRTABLE_TABLE_NAME,
   },
-  images: {
-    domains: ['cdn.shopify.com']
-  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,322 +1,187 @@
-import gql from "graphql-tag";
-import Link from "next/link";
-import apolloClient from "../gql/apolloClient";
-import React, { useState, useEffect } from "react";
-import Router from "next/router";
+import Head from "next/head";
+import Link from 'next/link'
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { client } from "../utils/shopify";
+import { createMedia } from "@artsy/fresnel";
+import { setCookies } from 'cookies-next';
+import { getCookie } from 'cookies-next';
+import SEO from "../components/SEO";
+import {
+  Button,
+  Popup,
+  Container,
+  Divider,
+  Grid,
+  Card,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Segment,
+  Sidebar,
+  Visibility,
+} from "semantic-ui-react";
+import charity from '../assets/charity';
+import axios from 'axios';
 
-const Home = ({ projects }) => {
-  console.log(projects, "projects");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [phone, setPhone] = useState("");
-
-  const [email, setEmail] = useState("");
-  const [emails, setEmails] = useState("");
-
-  const [password, setPassword] = useState("");
-  const [passwords, setPasswords] = useState("");
-
-  const [isLogin, setIsLogin] = useState("false");
-  const [isRegister, setRegister] = useState("");
-
-  const handleLogin = async () => {
-    const frmdetails = {
-      email: email,
-      password: password,
-    };
-    login(frmdetails);
-    setEmail("");
-    setPassword("");
-
-    // document.getElementById("loginForm").reset();
-  };
-
-  const handleRegister = async () => {
-    const frmdetails = {
-      firstName: firstName,
-      lastName: lastName,
-      phone: phone,
-      email: emails,
-      password: passwords,
-    };
-    register(frmdetails);
-    setFirstName("");
-    setLastName("");
-    setPhone("");
-    setEmails("");
-    setPasswords("");
-  };
-
-  const login = async (frmdetails) => {
-    console.log(frmdetails, "nssggbg");
-    const { data, error } = await apolloClient.mutate({
-      mutation: LOGIN_QUERY,
-      variables: {
-        email: frmdetails.email,
-        password: frmdetails.password,
-      },
-    });
-
-    if (data) {
-      console.log(data, error, "data");
-      if (data.customerAccessTokenCreate.customerAccessToken.accessToken) {
-        setIsLogin("true");
-        customerData(data.customerAccessTokenCreate.customerAccessToken.accessToken);
-        localStorage.setItem(
-          "accessToken",
-          data.customerAccessTokenCreate.customerAccessToken.accessToken
-        );
-        Router.push("/product");
-      }
+/* Heads up!
+ * HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled
+ * components for such things.
+ */
+const HomepageHeading = ({ mobile }) => (
+  <Container text>
+    <Header
+      as="h1"
+      content="Imagine-a-Company"
+      inverted
+      style={{
+        fontSize: mobile ? "2em" : "4em",
+        fontWeight: "normal",
+        marginBottom: 0,
+        marginTop: mobile ? "1.5em" : "3em",
+      }}
+    />
+    <SEO
+      openGraphType="website"
+      schemaType="article"
+      title="The Fate of Empires"
+      description="The only thing we learn from history, it has been said, 'is that men never learn from history'..."
+    />
+    <Header
+      as="h2"
+      content="Do whatever you want when you want to."
+      inverted
+      style={{
+        fontSize: mobile ? "1.5em" : "1.7em",
+        fontWeight: "normal",
+        marginTop: mobile ? "0.5em" : "1.5em",
+      }}
+    />
+    <Button primary size="huge">
+      Get Started
+      <Icon name="right arrow" />
+    </Button>
+    <Button primary size="huge" onClick={() => {
+      // setCookies('charity', 'care');
+      window.location.reload(true)
+    }}>
+      {/* Set Care */}
+    </Button>
+    <Button primary size="huge" onClick={() => {
+      setCookies('charity', 'home');
+      window.location.reload(true)
+    }}>
+      Set home
+    </Button>
+    <Link key="beauty" href="/beauty"><Button primary size="huge">
+    Beauty
+    </Button></Link>
+    <Link key="Jewelry" href="/jewelry"><Button primary size="huge">
+    Jewelry
+    </Button></Link>
+    {
+    
+    typeof window !== 'undefined' ? window.localStorage.setItem("daydream","sheetal") : ''
+      
     }
+    <Button>{typeof window !== 'undefined' ? window.localStorage.getItem("daydream") : 'No local staorage set'}</Button>
+    {/* <Link key="products" href="/productlisting?type=jewelry"><Button primary size="huge">
+    All Products
+    </Button></Link> */}
+  </Container>
+);
 
-    return {
-      props: {
-        projects: data,
-      },
-    };
-  };
-
-  const register = async (frmdetails) => {
-    console.log(frmdetails, "nssggbg");
-    const { data, error } = await apolloClient.mutate({
-      mutation: REGISTRATION_QUERY,
-      variables: {
-        firstName: frmdetails.firstName,
-        lastName: frmdetails.lastName,
-        email: frmdetails.email,
-        password: frmdetails.password,
-        phone: frmdetails.phone,
-        acceptsMarketing: false,
-      },
-    });
-
-    if (data) {
-      console.log(data, error, "data REGISTRATION_QUERY");
-      if (data.customerCreate.customer) {
-        console.log(data.customerCreate.customer, "here");
-        setRegister("Register Successfully");
-        // Router.push("/product");
-      } else {
-        if (data.customerCreate.customerUserErrors.length > 0) {
-          setRegister(data.customerCreate.customerUserErrors[0].message);
-        }
-      }
-    }
-
-    return {
-      props: {
-        projects: data,
-      },
-    };
-  };
-
-  const customerData = async (frmdetails) => {
-    console.log(frmdetails, "nssggbg");
-    const { data, error } = await apolloClient.query({
-      query: CUSTOMER_QUERY,
-      variables: {
-        customerAccessToken: frmdetails
-      },
-    });
-
-    if (data) {
-      console.log(data, error, "data");
-      localStorage.setItem("customer", JSON.stringify(data.customer));
-     
-    }
-
-    return {
-      props: {
-        projects: data,
-      },
-    };
-  };
-
-  return (
-    <>
-      <h2>{isLogin}</h2>
-      <h2>{isRegister}</h2>
-      <div className="container" style={{ display: "flex", justifyContent:"center" }}>
-        <div>
-          <h2>LOGIN</h2>
-
-          <input
-            type="email"
-            name="email"
-            value={email}
-            placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <br></br>
-          <br></br>
-
-          <input
-            type="password"
-            name="password"
-            value={password}
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-                    <br></br>
-          <br></br>
-
-          <button type="submit" onClick={handleLogin}>
-            Login
-          </button>
-        </div>
-    &nbsp; &nbsp; &nbsp; &nbsp; 
-        <div style={{ display: "inlineBlock" }}>
-          <h2>REGISTER</h2>
-          <input
-            type="text"
-            name="firstName"
-            placeholder="First Name"
-            value={firstName}
-            onChange={(e) => {
-              setFirstName(e.target.value);
-            }}
-          />
-           <br></br>
-          <br></br>
-
-          <input
-            type="text"
-            name="lastName"
-            placeholder="Last Name"
-            value={lastName}
-            onChange={(e) => {
-              setLastName(e.target.value);
-            }}
-          />
-           <br></br>
-          <br></br>
-          <input
-            type="text"
-            name="phone"
-            placeholder="Phone"
-            onChange={(e) => {
-              setPhone(e.target.value);
-            }}
-          />
-           <br></br>
-          <br></br>
-          <input
-            type="email"
-            name="email"
-            value={emails}
-            placeholder="Email"
-            onChange={(e) => setEmails(e.target.value)}
-          />
-           <br></br>
-          <br></br>
-          <input
-            type="password"
-            name="password"
-            value={passwords}
-            placeholder="Password"
-            onChange={(e) => setPasswords(e.target.value)}
-          />
-           <br></br>
-          <br></br>
-
-          <button type="submit" onClick={handleRegister}>
-            Register
-          </button>
-        </div>
-      </div>
-    </>
-  );
+HomepageHeading.propTypes = {
+  mobile: PropTypes.bool,
 };
 
-// const PROJECTS_QUERY = gql`
-//   query {
-//     collectionByHandle(handle: "frontpage") {
-//       title
-//       products(first: 25) {
-//         edges {
-//           node {
-//             id
-//             title
-//             handle
-//             priceRange {
-//               minVariantPrice {
-//                 amount
-//               }
-//             }
-//             images(first: 5) {
-//               edges {
-//                 node {
-//                   originalSrc
-//                   altText
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
-
-const LOGIN_QUERY = gql`
-  mutation ($email: String!, $password: String!) {
-    customerAccessTokenCreate(input: { email: $email, password: $password }) {
-      customerAccessToken {
-        accessToken
-        expiresAt
-      }
-      customerUserErrors {
-        code
-        field
-        message
-      }
-    }
-  }
-`;
-
-const REGISTRATION_QUERY = gql`
-  mutation (
-    $firstName: String!
-    $lastName: String!
-    $email: String!
-    $password: String!
-    $phone: String!
-    $acceptsMarketing: Boolean!
-  ) {
-    customerCreate(
-      input: {
-        firstName: $firstName
-        lastName: $lastName
-        email: $email
-        phone: $phone
-        password: $password
-        acceptsMarketing: $acceptsMarketing
-      }
-    ) {
-      customer {
-        firstName
-        lastName
-        id
-        email
-        updatedAt
-      }
-      customerUserErrors {
-        code
-        field
-        message
-      }
-    }
-  }
-`;
-
-const CUSTOMER_QUERY = gql`
-  query ($customerAccessToken: String!) {
-    customer(customerAccessToken: $customerAccessToken) {
-      firstName
-      lastName
-      email
-    }
-  }
-`;
+const Home = (props) =>
+  console.log(props) || (
+    <>
+      <Segment
+        inverted
+        textAlign="center"
+        style={{ minHeight: props.isMobile ? 300 : 700 }}
+        vertical
+      >
+        <HomepageHeading mobile={props.isMobile} />
+      </Segment>
+      <Segment vertical style={{ padding: "2em" }}>
+        <Grid container stackable verticalAlign="middle">
+          <Grid.Row>
+            <Header
+              content="Best Sales Product"
+              as="h2"
+              style={{
+                fontSize: props.isMobile ? "1.5em" : "1.7em",
+                fontWeight: "normal",
+              }}
+            />
+            <Card.Group itemsPerRow={props.isMobile ? 3 : 4}>
+              {props.products?.map(function(product, idx){
+                const charityArr = props.charity;
+                // const type = getCookie('charity') ? getCookie('charity') : 'care';
+                let message
+                // let message = charityArr[type].desc;
+                // message = message.replace('[[unit]]', charityArr[type].unit);
+                // message = message.replace('[[price]]', charityArr[type].price);
+                
+                  return (console.log(product) || (
+                    <Link key={product.id} href={`product/${product.id}`}>
+                      <Card raised>
+                        <Image src={product.images[0].src} wrapped ui={false} />
+                        <Card.Content>
+                          <Header sub>
+                            <Popup
+                              trigger={<div>{product.title}</div>}
+                              content={product.title}
+                              position="top left"
+                            />
+                          </Header>
+                          <Card.Meta>Joined in 2016</Card.Meta>
+                          <Card.Description>
+                            Daniel is a comedian living in Nashville.
+                          </Card.Description>
+                          <Card.Description>
+                            {message}{product.price}{product.productType}
+                          </Card.Description>
+                        </Card.Content>
+                      </Card>
+                     </Link>
+                  ))
+                  })}
+            </Card.Group>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    </>
+  );
 export default Home;
+
+export const getStaticProps = async (context) => {
+  const products = await client.product.fetchAll();
+  const infos = await client.shop.fetchInfo();
+  const policies = await client.shop.fetchPolicies();
+  let charityStoredArr = typeof window !== 'undefined' && window.localStorage.getItem("charityStored") ? window.localStorage.getItem("charityStored") : ''
+  if(typeof window !== 'undefined' && window.localStorage.getItem("charityStored")){
+    console.log('testing local storage setted');
+  }
+  typeof window !== 'undefined' ? window.localStorage.setItem("daydream1","sheetal") : ''
+  if(!charityStoredArr) {
+  // const {data} = await axios.get(process.env.NETLIFY_URL + '/api1/charity');
+  // charityStoredArr = data;
+  console.log('testing local storage setting');
+  typeof window !== 'undefined' ? window.localStorage.setItem("charityStored") : JSON.stringify(charityStoredArr);
+  }
+  return {
+    props: {
+      infos: JSON.parse(JSON.stringify(infos)),
+      policies: JSON.parse(JSON.stringify(policies)),
+      products: JSON.parse(JSON.stringify(products)),
+      // charity: JSON.parse(JSON.stringify(charityStoredArr)),
+      revalidate: 60, // In seconds
+    },
+  };
+};
